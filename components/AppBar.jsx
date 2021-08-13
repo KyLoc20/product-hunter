c2cefc304ee12f781e7b4a7b49675f23d4cfbe5b
liuzeduo <liuzeduo1234@gmail.com>
2021-08-13 23:44:20 +0800

    rewrite: start to reorganize  directory structure

--- components/AppBar.jsx
+++ components/AppBar.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
-import Icon from "../../ui/Icon/Icon";
+import Icon from "@/ui/Icon/Icon";
 import Button from "../../ui/Button/Button";
 import SearchInput from "./SearchInput";
 const BasicAppBar = styled("section")`
